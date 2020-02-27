af2fd6207d0e9f43ae98d323fc25d6f4988a2a46
Megan Venetianer <megan.venetianer@gmail.com>
2020-02-26 18:12:41 -0700

    initial HTML wireframe

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+// ---------- css ----------
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
+// ---------- images ----------
+import './images/mountains-lake.jpg';
+import './images/snow-mountains.jpg'
